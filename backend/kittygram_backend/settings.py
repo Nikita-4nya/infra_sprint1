52ffa9cca11206d1a680e8e643ca40524f8f1d54
Nikita Chetvernya <115979983+Nikita-4nya@users.noreply.github.com>
2023-07-28 20:53:25 +0500

    Changed DEBUG and ALLOWED_HOSTS.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', False) == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '4nya-kittygram.hopto.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
